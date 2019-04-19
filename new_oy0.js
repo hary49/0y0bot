7f37e91975f350ac0f5feabe6e42a2d2ee3863ac
Amin <amin4udin@gmail.com>
2019-04-19 17:20:21 +0700

    initial

---
+++ new_oy0.js
@@ -0,0 +1,163 @@
+var _0x4470 = [
+  "1555660584993|dea:1|mwhp:1|t3c:0|epba:0|pnna:0|srma:1|rst2:1|phbb:0|pnni:2|srmi:1|wcta:1|wtea:1|cdr2:1|popl:0|nhi2:1|bann:1|sbma:1|rae:1|gmfh:1|prep:1|fbtn:1|htab:1|hbna:1|absb:0|se1:0|sbmi:1|se3:0|se2:0|se5:11|se4:1|rtsa:0|se6:17|cour:1|gsra:1|rtsi:1|hdab:1|slpr:0|rba:0|cdr:0|ipw:0|rd:1|ipw2:0|pppp:0|lzpi:1|npfi:1|uprc:1|rbl:1|hmva:0|rsa:0|dww2:0|deal:0|pnpd:1|rcua:1|rsi:0|gsti:1|dwep:2|otp4:1|urha:1|ppa:2|ona:0|svh:1|stc2:1|urhi:1|ppi:2|gsta:0|gstc:0|nlab:1|asa:1|cr:1|rts:0|nlp:1|mjo:1|onab:1|asi:1|wtei:1|asei:1|bsba:2|aca:1|bea:1|wtib:3|hbri:0|lyr:0|aci:0|scta:0|tspk:1|hste:1|tspi:0|tspj:0|DWWS:1|a2hs:1|pfri:0|brch:4|test:1|raab:1|sink:0|aswp:1|shli:0|hrr:0|hrt:1|riab:0|hbad:0|hbi:0|rcui:1|idl:0|sbpa:0|stcl:1|sbpi:0|sinc:1|shla:0|brea:1|idum:0|lpta:1|lpti:1|ffab:1|his2:0|hbci:1|pst:1|stfi:2|pce:1|stft:2|omue:0|brei:1|hsei:1|hbsi:0|sold:1|hbca:1|stfa:1|stfb:1|home:0|scti:0|otab:1|cvis:0|gsa:1|dwhp:0|hbsa:1|gsi:1|rasl:0|locr:0|nhpi:0|obai:1|dbad:1|nrca:1|epa:2|nrci:1|hlis:0|epi:0|epn:2|fbb:0|trab:1|rmo2:1|niab:0|lbht:0|weng:0|shpa:1|hppl:0|hsfa:1|sls:1|shpi:1|apei:1|loc:0|phli:0|gpwa:0|nsl:1|prpa:0|saet:1|nhba:1|gpwi:0|nrfa:0|nbwa:0|prpi:0|hbi2:0|mtlp:1|saea:1|mrc:1|blh:1|cpab:1|hpsa:0|vct:1|octt:1|phb:1|hpsi:0|cadd:1|nsfa:1|oban:1|spc2:1|smla:0|sfni:2|auto:1|uiab:1|pvis:0|wtab:3|shel:1|ndlp:0|hmpi:1|his:0|rmo:1|bdpi:1|pbra:1|sos:2|logn:1|rms:1|uaab:1|papg:1|bdpa:1|pbri:1|nob2:1|swar:1|aowt:1|spc:1|pioi:0|rms2:0|trCl:1|nhbi:1|lbh:1|nrfi:0|nbwi:0|paom:0|lsc:1|pdhi:0|tsb:0|lsc2:0|diei:1|dmme:1|diea:1|dte:1|acsi:1|nuom:0|adum:1|pdha:1|uhps:0|mwen:0|nobs:1|efa:1|hpwa:0|fbb2:0|sra:2|reca:1|BnTc:0|paab:0|mwep:2|ngst:1|hpwi:1|ltsc:1|reci:1|jbei:1|piab:0|aimg:1|avgp:0|ffib:1|mww2:1|ioab:0|hpfd:1|srz:1|socp:0|plwc:1",
+  "QmpDZlRxZWo2UkZ5M3pSeHZ5NW46bi13NHN0ZTV5V1I1aGpCUVVHOUM=",
+  "chrome",
+  "Nexus\x206",
+  "Masukan\x20kode\x20otp\x20:\x20",
+  "Masukan\x20refferal\x20:\x20",
+  "format",
+  "HH:mm:ss",
+  "first_name",
+  "node-fetch",
+  "delay",
+  "readline-sync",
+  "./lib/colors",
+  "username-generator",
+  "useragent-generator",
+  "log",
+  "Panggil\x20w\x20Amin\x20Tamvan",
+  "#####################",
+  "question",
+  "Masukan\x20no\x20hp\x20:\x20",
+  "https://api.oyorooms.com/v2/users/generate_otp?phone=",
+  "&nod=4&intent=login&sms_auto_retrieval=false&country_code=%2B62&version=20215&partner_app_version=20215&android_id=7acfb18bcd6f99e7&idfa=&sid=1555663216720",
+  "get",
+  "application/json",
+  "default",
+  "Basic\x20QmpDZlRxZWo2UkZ5M3pSeHZ5NW46bi13NHN0ZTV5V1I1aGpCUVVHOUM=",
+  "gzip",
+  "androidPhone",
+  "61.0.0",
+  "7.1.2",
+  "api.oyorooms.com",
+  "Keep-Alive",
+  "then",
+  "json",
+  "catch",
+  "+62",
+  "7acfb18bcd6f99e7",
+  "android",
+  "generateUsername",
+  "gcm",
+  "https://api.oyorooms.com/v2/users/new_sign_up?additional_fields=ab_service_data&handset=Genymotion%2C%20Custom%20Phone%20-%206.0.0%20-%20API%2023%20-%20768x1280&version=20215&partner_app_version=20215&android_id=7acfb18bcd6f99e7&idfa=&sid=1555663216720\x20",
+  "post",
+  "stringify"
+];
+(function(_0x6fc67c, _0x595207) {
+  var _0x50d128 = function(_0x2232dc) {
+    while (--_0x2232dc) {
+      _0x6fc67c["push"](_0x6fc67c["shift"]());
+    }
+  };
+  _0x50d128(++_0x595207);
+})(_0x4470, 0x1b7);
+var _0x476e = function(_0x2552a5, _0x5ecc57) {
+  _0x2552a5 = _0x2552a5 - 0x0;
+  var _0x50bfe9 = _0x4470[_0x2552a5];
+  return _0x50bfe9;
+};
+const fetch = require(_0x476e("0x0"));
+const delay = require(_0x476e("0x1"));
+const readline = require(_0x476e("0x2"));
+const colors = require(_0x476e("0x3"));
+const moment = require("moment");
+const fs = require("fs");
+const UsernameGenerator = require(_0x476e("0x4"));
+const ua = require(_0x476e("0x5"));
+console[_0x476e("0x6")]("#####################");
+console["log"](_0x476e("0x7"));
+console[_0x476e("0x6")](_0x476e("0x8"));
+console["log"]("");
+console["log"]("");
+const phone = readline[_0x476e("0x9")](_0x476e("0xa"));
+console[_0x476e("0x6")]("");
+console[_0x476e("0x6")]("");
+const functionRegister = _0x26a9d5 =>
+  new Promise((_0x5bb242, _0x3ccfc6) => {
+    fetch(_0x476e("0xb") + phone + _0x476e("0xc"), {
+      method: _0x476e("0xd"),
+      headers: {
+        "Content-Type": _0x476e("0xe"),
+        OYO_AB_CONFIG:
+          "1555660584993|dea:1|mwhp:1|t3c:0|epba:0|pnna:0|srma:1|rst2:1|phbb:0|pnni:2|srmi:1|wcta:1|wtea:1|cdr2:1|popl:0|nhi2:1|bann:1|sbma:1|rae:1|gmfh:1|prep:1|fbtn:1|htab:1|hbna:1|absb:0|se1:0|sbmi:1|se3:0|se2:0|se5:11|se4:1|rtsa:0|se6:17|cour:1|gsra:1|rtsi:1|hdab:1|slpr:0|rba:0|cdr:0|ipw:0|rd:1|ipw2:0|pppp:0|lzpi:1|npfi:1|uprc:1|rbl:1|hmva:0|rsa:0|dww2:0|deal:0|pnpd:1|rcua:1|rsi:0|gsti:1|dwep:2|otp4:1|urha:1|ppa:2|ona:0|svh:1|stc2:1|urhi:1|ppi:2|gsta:0|gstc:0|nlab:1|asa:1|cr:1|rts:0|nlp:1|mjo:1|onab:1|asi:1|wtei:1|asei:1|bsba:2|aca:1|bea:1|wtib:3|hbri:0|lyr:0|aci:0|scta:0|tspk:1|hste:1|tspi:0|tspj:0|DWWS:1|a2hs:1|pfri:0|brch:4|test:1|raab:1|sink:0|aswp:1|shli:0|hrr:0|hrt:1|riab:0|hbad:0|hbi:0|rcui:1|idl:0|sbpa:0|stcl:1|sbpi:0|sinc:1|shla:0|brea:1|idum:0|lpta:1|lpti:1|ffab:1|his2:0|hbci:1|pst:1|stfi:2|pce:1|stft:2|omue:0|brei:1|hsei:1|hbsi:0|sold:1|hbca:1|stfa:1|stfb:1|home:0|scti:0|otab:1|cvis:0|gsa:1|dwhp:0|hbsa:1|gsi:1|rasl:0|locr:0|nhpi:0|obai:1|dbad:1|nrca:1|epa:2|nrci:1|hlis:0|epi:0|epn:2|fbb:0|trab:1|rmo2:1|niab:0|lbht:0|weng:0|shpa:1|hppl:0|hsfa:1|sls:1|shpi:1|apei:1|loc:0|phli:0|gpwa:0|nsl:1|prpa:0|saet:1|nhba:1|gpwi:0|nrfa:0|nbwa:0|prpi:0|hbi2:0|mtlp:1|saea:1|mrc:1|blh:1|cpab:1|hpsa:0|vct:1|octt:1|phb:1|hpsi:0|cadd:1|nsfa:1|oban:1|spc2:1|smla:0|sfni:2|auto:1|uiab:1|pvis:0|wtab:3|shel:1|ndlp:0|hmpi:1|his:0|rmo:1|bdpi:1|pbra:1|sos:2|logn:1|rms:1|uaab:1|papg:1|bdpa:1|pbri:1|nob2:1|swar:1|aowt:1|spc:1|pioi:0|rms2:0|trCl:1|nhbi:1|lbh:1|nrfi:0|nbwi:0|paom:0|lsc:1|pdhi:0|tsb:0|lsc2:0|diei:1|dmme:1|diea:1|dte:1|acsi:1|nuom:0|adum:1|pdha:1|uhps:0|mwen:0|nobs:1|efa:1|hpwa:0|fbb2:0|sra:2|reca:1|BnTc:0|paab:0|mwep:2|ngst:1|hpwi:1|ltsc:1|reci:1|jbei:1|piab:0|aimg:1|avgp:0|ffib:1|mww2:1|ioab:0|hpfd:1|srz:1|socp:0|plwc:1",
+        SEGMENT_CONFIG: _0x476e("0xf"),
+        access_token:
+          "QmpDZlRxZWo2UkZ5M3pSeHZ5NW46bi13NHN0ZTV5V1I1aGpCUVVHOUM=",
+        Authorization: _0x476e("0x10"),
+        "Accept-Encoding": _0x476e("0x11"),
+        "User-Agent": ua["chrome"][_0x476e("0x12")]({
+          version: _0x476e("0x13"),
+          androidVersion: _0x476e("0x14"),
+          device: "Nexus\x206"
+        }),
+        Host: _0x476e("0x15"),
+        Connection: _0x476e("0x16")
+      }
+    })
+      [_0x476e("0x17")](_0x313b3c => _0x313b3c[_0x476e("0x18")]())
+      [_0x476e("0x17")](_0x37f4ad => {
+        _0x5bb242(_0x37f4ad);
+      })
+      [_0x476e("0x19")](_0x15b349 => _0x3ccfc6(_0x15b349));
+  });
+const functionConfirm = (_0x2381b7, _0x2ccf35) =>
+  new Promise((_0x54315b, _0x437621) => {
+    const _0x9e702c = {
+      truecaller: ![],
+      code: _0x2381b7,
+      country_code: _0x476e("0x1a"),
+      device_id: _0x476e("0x1b"),
+      device_type: _0x476e("0x1c"),
+      email: UsernameGenerator[_0x476e("0x1d")]() + "@gmail.com",
+      id: 0x0,
+      idfa: "",
+      name: UsernameGenerator[_0x476e("0x1d")](),
+      phone: phone,
+      push_type: _0x476e("0x1e"),
+      referral_code: _0x2ccf35,
+      token: "",
+      updated_at: 0x0
+    };
+    fetch(_0x476e("0x1f"), {
+      method: _0x476e("0x20"),
+      body: JSON[_0x476e("0x21")](_0x9e702c),
+      headers: {
+        "Content-Type": _0x476e("0xe"),
+        OYO_AB_CONFIG: _0x476e("0x22"),
+        SEGMENT_CONFIG: _0x476e("0xf"),
+        access_token: _0x476e("0x23"),
+        Authorization:
+          "Basic\x20QmpDZlRxZWo2UkZ5M3pSeHZ5NW46bi13NHN0ZTV5V1I1aGpCUVVHOUM=",
+        "Accept-Encoding": _0x476e("0x11"),
+        "User-Agent": ua[_0x476e("0x24")][_0x476e("0x12")]({
+          version: _0x476e("0x13"),
+          androidVersion: "7.1.2",
+          device: _0x476e("0x25")
+        }),
+        Host: _0x476e("0x15"),
+        Connection: _0x476e("0x16")
+      }
+    })
+      [_0x476e("0x17")](_0x380c4c => _0x380c4c[_0x476e("0x18")]())
+      [_0x476e("0x17")](_0x226034 => {
+        _0x54315b(_0x226034);
+      })
+      [_0x476e("0x19")](_0x9ba8b => _0x437621(_0x9ba8b));
+  });
+(async () => {
+  const _0xa7c5a0 = await functionRegister();
+  const _0x283757 = await readline[_0x476e("0x9")](_0x476e("0x26"));
+  const _0x56dcd1 = await readline[_0x476e("0x9")](_0x476e("0x27"));
+  const _0x5c70ae = await functionConfirm(_0x283757, _0x56dcd1);
+  console[_0x476e("0x6")](
+    "[" +
+      "\x20" +
+      moment()[_0x476e("0x28")](_0x476e("0x29")) +
+      "\x20" +
+      "]" +
+      "\x20" +
+      "Sukses\x20register\x20with\x20name\x20:" +
+      "\x20" +
+      _0x5c70ae[_0x476e("0x2a")]
+  );
+})();
